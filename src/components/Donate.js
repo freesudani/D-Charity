861f6a74122248d9ee38b35557837420e922a247
freesudani <mohamed.dahiya@hotmail.com>
2022-01-29 09:45:05 +0700

    Modify Donate Section

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -1,6 +1,6 @@
 import React from "react";
 import classes from "./Donate.module.css";
-import ButtonSmall from "../UI/ButtonSmall";
+import ButtonMedium from "../UI/ButtonMedium";
 import DonateIcon from "../UI/DonateIcon";
 
 const Donate = () => {
@@ -10,21 +10,21 @@ const Donate = () => {
         <form>
           <div className={classes.data}>
             <label htmlFor="name">Name</label>
-            <input id="name" type="text" />
+            <input id="name" type="text" placeholder="Full Name" />
           </div>
           <div className={classes.data}>
             <label htmlFor="email">E-Mail</label>
-            <input id="email" type="email" />
+            <input id="email" type="email" placeholder="E-mail" />
           </div>
           <div className={classes.data}>
             <label htmlFor="phone">Phone Number</label>
-            <input id="phone" type="tel" />
+            <input id="phone" type="tel" placeholder="Phone Number" />
           </div>
           <div className={classes.datasmall}>
             <label htmlFor="amount">Amount</label>
-            <input id="amount" type="number" />
+            <input id="amount" type="number" placeholder="$" />
           </div>
-          <ButtonSmall>Submit</ButtonSmall>
+          <ButtonMedium>Submit</ButtonMedium>
         </form>
       </div>
       <div className={classes.donateicon}>
